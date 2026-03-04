77dae08f09e1182d95a0c2ad6f04168a7b00806b
Paulo Albuquerque <paulo.albuquerque@gmail.com>
2026-03-04 01:18:27 +0000

    feat: tweak agent parameters

--- src/doctor_houseplant/agent.py
+++ src/doctor_houseplant/agent.py
@@ -1,15 +1,30 @@
 from importlib import resources as impresources
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents import LlmAgent
+from google.genai import types
 
 # Read the instruction provided with the package
 instruction = impresources.read_text(__package__, "INSTRUCTION.md")
 
+# Create our agent config,
+# use a low temperature for consistency
+# set safety settings to block any dangerous content.
+content_config = types.GenerateContentConfig(
+    temperature=0.1,  # Very low for consistency
+    safety_settings=[
+        types.SafetySetting(
+            category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold=types.HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        )
+    ],
+)
+
 # Create the agent with the instruction and other metadata.
-doctor_houseplant = Agent(
+doctor_houseplant = LlmAgent(
     model="gemini-2.5-flash",
     name="doctor_houseplant",  # Used by ADK internally
     description="Identifies plants and helps taking care of them.",
     instruction=instruction,
+    generate_content_config=content_config,
 )
 
 # Variable name that ADK tools look for (must be root_agent)
